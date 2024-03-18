3a32eebc9648c3388a73e5ea1adc2ea425c2472b
ayushMishra464 <158063230+ayushMishra464@users.noreply.github.com>
2024-03-18 23:49:39 +0530

    Update kruskal.cpp

--- .vscode/lab6/kruskal.cpp
+++ .vscode/lab6/kruskal.cpp
@@ -1,150 +1,93 @@
-#include <iostream>
-#include <chrono>
+// Kruskal's algorithm in C++
+
 #include <algorithm>
-#include <random>
+#include <iostream>
+#include <vector>
 using namespace std;
-using namespace std::chrono;
 
-const int MAX_VERTICES = 1000; 
+#define edge pair<int, int>
 
-struct Edge
-{
-    int source;
-    int destination;
-    int weight;
+class Graph {
+   private:
+  vector<pair<int, edge> > G;  // graph
+  vector<pair<int, edge> > T;  // mst
+  int *parent;
+  int V;  // number of vertices/nodes in graph
+   public:
+  Graph(int V);
+  void AddWeightedEdge(int u, int v, int w);
+  int find_set(int i);
+  void union_set(int u, int v);
+  void kruskal();
+  void print();
 };
+Graph::Graph(int V) {
+  parent = new int[V];
 
-struct DisjointSet
-{
-    int parent[MAX_VERTICES];
-    int rank[MAX_VERTICES];
+  //i 0 1 2 3 4 5
+  //parent[i] 0 1 2 3 4 5
+  for (int i = 0; i < V; i++)
+    parent[i] = i;
 
-    DisjointSet(int n)
-    {
-        for (int i = 0; i < n; ++i)
-        {
-            parent[i] = i;
-            rank[i] = 0;
-        }
-    }
-
-    int find(int u)
-    {
-        if (parent[u] != u)
-        {
-            parent[u] = find(parent[u]);
-        }
-        return parent[u];
-    }
-
-    void unionSets(int u, int v)
-    {
-        int rootU = find(u);
-        int rootV = find(v);
-
-        if (rank[rootU] < rank[rootV])
-        {
-            parent[rootU] = rootV;
-        }
-        else if (rank[rootU] > rank[rootV])
-        {
-            parent[rootV] = rootU;
-        }
-        else
-        {
-            parent[rootU] = rootV;
-            rank[rootV]++;
-        }
-    }
-};
-
-
-
-void Heapify(Edge edge[], int i, int N)
-{
-    int large = i;
-    int left = 2 * i + 1;
-    int right = 2 * i + 2;
-
-    if (left < N && edge[i].weight < edge[left].weight)
-    {
-        large = left;
-    }
-    if (right < N && edge[large].weight < edge[right].weight)
-    {
-        large = right;
-    }
-    if (large != i)
-    {
-        swap(edge[i], edge[large]);
-        Heapify(edge, large, N);
-    }
+  G.clear();
+  T.clear();
+}
+void Graph::AddWeightedEdge(int u, int v, int w) {
+  G.push_back(make_pair(w, edge(u, v)));
+}
+int Graph::find_set(int i) {
+  // If i is the parent of itself
+  if (i == parent[i])
+    return i;
+  else
+    // Else if i is not the parent of itself
+    // Then i is not the representative of his set,
+    // so we recursively call Find on its parent
+    return find_set(parent[i]);
 }
 
-void build_heap(Edge edge[], int n)
-{
-    for (int i = n / 2 - 1; i >= 0; --i)
-    {
-        Heapify(edge, i, n);
-    }
+void Graph::union_set(int u, int v) {
+  parent[u] = parent[v];
 }
-
-void kruskal(Edge edge[], int n, int **t)
-{
-    build_heap(edge, n);
-
-    DisjointSet d(n);
-
-    int edgeCount = 0;
-    int i = 0;
-
-    while (edgeCount < n - 1)
-    {
-        Edge E1 = edge[i];
-
-        int Source = d.find(E1.source);
-        int Dest = d.find(E1.destination);
-
-        if (Source != Dest)
-        {
-            t[edgeCount][0] = E1.source;
-            t[edgeCount][1] = E1.destination;
-            edgeCount++;
-            d.unionSets(Source, Dest);
-        }
-
-        i++;
+void Graph::kruskal() {
+  int i, uRep, vRep;
+  sort(G.begin(), G.end());  // increasing weight
+  for (i = 0; i < G.size(); i++) {
+    uRep = find_set(G[i].second.first);
+    vRep = find_set(G[i].second.second);
+    if (uRep != vRep) {
+      T.push_back(G[i]);  // add to tree
+      union_set(uRep, vRep);
     }
+  }
 }
-
-int main()
-{
-    int n;
-    cout << "choose the number of edges";
-    cin >> n;
-
-    int **t = new int*[n - 1];
-    for (int i = 0; i < n - 1; ++i) {
-        t[i] = new int[2];
-    }
-
-    Edge edge[n];
-    for (int i = 0; i < n; i++) {
-        cout << "enter source then destination and then weight";
-        cin >> edge[i].source >> edge[i].destination >> edge[i].weight;
-    }
-
-    kruskal(edge, n, t);
-
-    cout << "Chosen Edges in the Minimum Spanning Tree (MST):" << endl;
-    for (int i = 0; i < n - 1; ++i) {
-        cout << "Edge: " << t[i][0] << " - " << t[i][1] << endl;
-    }
-
-    for (int i = 0; i < n - 1; ++i) {
-        delete[] t[i];
-    }
-    delete[] t;
-
-    return 0;
+void Graph::print() {
+  cout << "Edge :"
+     << " Weight" << endl;
+  for (int i = 0; i < T.size(); i++) {
+    cout << T[i].second.first << " - " << T[i].second.second << " : "
+       << T[i].first;
+    cout << endl;
+  }
+}
+int main() {
+  Graph g(6);
+  g.AddWeightedEdge(0, 1, 4);
+  g.AddWeightedEdge(0, 2, 4);
+  g.AddWeightedEdge(1, 2, 2);
+  g.AddWeightedEdge(1, 0, 4);
+  g.AddWeightedEdge(2, 0, 4);
+  g.AddWeightedEdge(2, 1, 2);
+  g.AddWeightedEdge(2, 3, 3);
+  g.AddWeightedEdge(2, 5, 2);
+  g.AddWeightedEdge(2, 4, 4);
+  g.AddWeightedEdge(3, 2, 3);
+  g.AddWeightedEdge(3, 4, 3);
+  g.AddWeightedEdge(4, 2, 4);
+  g.AddWeightedEdge(4, 3, 3);
+  g.AddWeightedEdge(5, 2, 2);
+  g.AddWeightedEdge(5, 4, 3);
+  g.kruskal();
+  g.print();
+  return 0;
 }
