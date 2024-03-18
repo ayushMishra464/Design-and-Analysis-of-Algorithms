c891832f668b94bf8ba612654153da79e1bf7f03
ayushMishra464 <158063230+ayushMishra464@users.noreply.github.com>
2024-03-18 20:04:08 +0530

    Update Assembly_line.cpp

--- Lab 7 /Assembly_line.cpp
+++ Lab 7 /Assembly_line.cpp
@@ -1,104 +1,39 @@
-
 #include <iostream>
-#include <vector>
 using namespace std;
+#define n 3
 
-#define NUM_STATIONS 2
-#define NUM_LINES 2
+int min(int a, int b)
+{ 
+	return a < b ? a : b; 
+} 
 
-int assemblyLineScheduling(int a[][NUM_STATIONS], int t[][NUM_STATIONS - 1], int *e, int *x, vector<vector<int>>& linePath) {
-    int T1[NUM_STATIONS], T2[NUM_STATIONS];
-    vector<vector<int>> F(NUM_LINES, vector<int>(NUM_STATIONS));
-    vector<vector<int>> L(NUM_LINES, vector<int>(NUM_STATIONS));
+int assembly(int a[][n],int t[][n], int *e, int *x) 
+{ 
+	int T1[n], T2[n], i; 
 
-    F[0][0] = e[0] + a[0][0];
-    F[1][0] = e[1] + a[1][0];
+	// adding base case times
+	T1[0] = e[0] + a[0][0]; 
+	T2[0] = e[1] + a[1][0]; 
 
-    for (int j = 1; j < NUM_STATIONS; j++) {
-        if ((F[0][j - 1] + a[0][j]) <= (F[1][j - 1] + t[0][j - 1] + a[0][j])) {
-            F[0][j] = F[0][j - 1] + a[0][j];
-            L[0][j] = 0;
-        } else {
-            F[0][j] = F[1][j - 1] + t[0][j - 1] + a[0][j];
-            L[0][j] = 1;
-        }
+	// Filling the dp tables T1[] and T2[] using recursive relations
+	for (i = 1; i < n; i++) 
+	{ 
+		T1[i] = min(T1[i - 1] + a[0][i], T2[i - 1] + t[1][i] + a[0][i]); 
+		T2[i] = min(T2[i - 1] + a[1][i], T1[i - 1] + t[0][i] + a[1][i]); 
+	} 
 
-        if ((F[1][j - 1] + a[1][j]) <= (F[0][j - 1] + t[1][j - 1] + a[1][j])) {
-            F[1][j] = F[1][j - 1] + a[1][j];
-            L[1][j] = 1;
-        } else {
-            F[1][j] = F[0][j - 1] + t[1][j - 1] + a[1][j];
-            L[1][j] = 0;
-        }
-    }
+	// finding final times and returning the minimum value 
+	return min(T1[n - 1] + x[0], T2[n - 1] + x[1]); 
+} 
 
-    int f;
-    if ((F[0][NUM_STATIONS - 1] + x[0]) <= (F[1][NUM_STATIONS - 1] + x[1])) {
-        f = F[0][NUM_STATIONS - 1] + x[0];
-        linePath[0][NUM_STATIONS - 1] = 0;
-    } else {
-        f = F[1][NUM_STATIONS - 1] + x[1];
-        linePath[0][NUM_STATIONS - 1] = 1;
-    }
+// Driver Code
+int main() 
+{ 
+	int a[][n] = {{5, 4, 3}, {2, 3, 7}}; 
+	int t[][n] = {{0, 2, 2}, {0, 1, 1}}; 
+	int e[] = {3, 2}, x[] = {3, 4}; 
 
-    for (int j = NUM_STATIONS - 2; j >= 0; j--) {
-        linePath[0][j] = L[linePath[0][j + 1]][j + 1];
-    }
+	cout<<"The minimum time taken is: "<<assembly(a, t, e, x); 
 
-    return f;
-}
-
-void printPath(const vector<vector<int>>& linePath) {
-    cout << "Path: ";
-    for (int j = 0; j < NUM_STATIONS; j++) {
-        cout << "Station " << j + 1 << " on Line " << linePath[0][j] + 1 << " -> ";
-    }
-    cout << "Exit\n";
-}
-
-int main() {
-    int a[NUM_LINES][NUM_STATIONS];  
-    int t[NUM_LINES][NUM_STATIONS - 1]; 
-    int e[NUM_LINES];  
-    int x[NUM_LINES]; 
-  
-    cout << "Enter processing times for stations:\n";
-    for (int i = 0; i < NUM_LINES; ++i) {
-        for (int j = 0; j < NUM_STATIONS; ++j) {
-            cout << "Processing time for Line " << i + 1 << ", Station " << j + 1 << ": ";
-            cin >> a[i][j];
-        }
-    }
-
-    
-    cout << "Enter transfer times between stations:\n";
-    for (int i = 0; i < NUM_LINES; ++i) {
-        for (int j = 0; j < NUM_STATIONS - 1; ++j) {
-            cout << "Transfer time for Line " << i + 1 << ", Station " << j + 1 << " to Station " << j + 2 << ": ";
-            cin >> t[i][j];
-        }
-    }
-
-   
-    cout << "Enter entry times for lines:\n";
-    for (int i = 0; i < NUM_LINES; ++i) {
-        cout << "Entry time for Line " << i + 1 << ": ";
-        cin >> e[i];
-    }
-
-    
-    cout << "Enter exit times for lines:\n";
-    for (int i = 0; i < NUM_LINES; ++i) {
-        cout << "Exit time for Line " << i + 1 << ": ";
-        cin >> x[i];
-    }
-
-    vector<vector<int>> linePath(1, vector<int>(NUM_STATIONS));
-
-    int minCost = assemblyLineScheduling(a, t, e, x, linePath);
-
-    cout << "Minimum cost: " << minCost << endl;
-    printPath(linePath);
-
-    return 0;
-}
+	return 0; 
+} 
